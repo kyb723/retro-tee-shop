15cccd11b8a791bbe121a77d00c5f6d922cbd474
kyb723 <annibaby0723@gmail.com>
2025-05-02 10:03:33 +0800

    Add files via upload

---
+++ mode.js
@@ -0,0 +1,9 @@
+function toggleMode() {
+  document.body.classList.toggle("light-mode");
+  localStorage.setItem("theme", document.body.classList.contains("light-mode") ? "light" : "dark");
+}
+window.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("theme") === "light") {
+    document.body.classList.add("light-mode");
+  }
+});
